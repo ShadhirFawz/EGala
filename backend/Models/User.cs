6a65dacb4d4b6fcf2cb6af82681224b326d2fa6f
Shadhir Fawz <it22237118@my.sliit.lk>
2025-10-15 20:15:02 +0530

    MongoDB config with auth request DTOs

---
+++ backend/Models/User.cs
@@ -0,0 +1,27 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; } = null!;
+
+        [BsonElement("email")]
+        public string Email { get; set; } = null!;
+
+        [BsonElement("passwordHash")]
+        public string PasswordHash { get; set; } = null!;
+
+        [BsonElement("role")]
+        public string Role { get; set; } = "Customer"; // Default role
+
+        [BsonElement("isActive")]
+        public bool IsActive { get; set; } = true;
+    }
+}
